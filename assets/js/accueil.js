7cf64c7cb2cc52b0b219ae76d1f4fe4d5ecee031
Kathryn Undercoffler <kathryn.grce@gmail.com>
2023-05-18 17:27:34 -0400

    FAIT : - JS pour les vidéos aléatoire sur la page d'accueil - Ajout de loop dans le balisage pour la vidéo

--- assets/js/accueil.js
+++ assets/js/accueil.js
@@ -3,19 +3,42 @@
  * @author Kate Undercoffler
 */
 
-nombreVideo: 3;
+// Code de https://stackoverflow.com/questions/5235145/changing-source-on-html5-video-tag
 
-// Assignation des écouteurs d'événements : 
-document.getElementById('video')
-.addEventListener('load', changerVideo);
 
 const objVisionneuse = {
- console.log("hello");
-    //Méthode : 
-    changerVideo: function (e) {
 
+    //Méthode : 
+    /**
+     * Choisir au hasard une vidéo quand la page initialise 
+     */
+    jouerVideo: function () {
+        console.log("hello");
+
+        // Choisir un numéro au hasard entre 1-3 
+        let intVideoHasard = Math.floor(Math.random() * 3 + 1);
+        console.log(intVideoHasard);
+
+        // Variable qui contiene le lien des videos
+        let strUrlVideo = "./assets/videos/video_" + intVideoHasard + ".mp4";
+
+        //Chercher l'ID de la video et créer le balisage source
+        let refVideo = document.querySelector('#video');
+        let refSource = document.createElement('source');
+
+        //Dans le balisage source, set l'attribute du lien de la video et le type
+        refSource.setAttribute('src', strUrlVideo);
+        refSource.setAttribute('type', 'video/mp4');
+
+        refVideo.appendChild(refSource);
+
+        refVideo.play();
     }
 
 };
 
-objVisionneuse.changerVideo();
+objVisionneuse.jouerVideo();
+
+// Assignation des écouteurs d'événements : 
+document.getElementById('video')
+    .addEventListener('load', objVisionneuse.jouerVideo);
