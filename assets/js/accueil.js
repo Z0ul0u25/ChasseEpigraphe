9dcb86e296d8f7ae31cb5ee617b6f799e776e90e
Kathryn Undercoffler <kathryn.grce@gmail.com>
2023-05-18 17:01:31 -0400

    Début de JS pour les videos

---
+++ assets/js/accueil.js
@@ -0,0 +1,21 @@
+/** 
+ *  JS pour la visionneuse des vidéo sur la page d'accueil
+ * @author Kate Undercoffler
+*/
+
+nombreVideo: 3;
+
+// Assignation des écouteurs d'événements : 
+document.getElementById('video')
+.addEventListener('load', changerVideo);
+
+const objVisionneuse = {
+ console.log("hello");
+    //Méthode : 
+    changerVideo: function (e) {
+
+    }
+
+};
+
+objVisionneuse.changerVideo();
