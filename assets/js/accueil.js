ec09e5d5fc57b4edb1b68af02fe72d7aac98ece9
Z0ul0u25 <philougourdo@gmail.com>
2023-05-22 13:11:06 -0400

    Les trois vidéos circulent une après l'autre

--- assets/js/accueil.js
+++ assets/js/accueil.js
@@ -2,7 +2,7 @@
  *  JS pour la visionneuse des vidéo sur la page d'accueil
  * @author Kate Undercoffler
  * @reviewer Philippe Gourdeau <2266603@csfoy.ca>
- * @version 1.1
+ * @version 1.5
 */
 
 /* --- ESLINT --- */
@@ -13,30 +13,40 @@
 
 const objVisionneuse = {
 
+    // Choisir un numéro au hasard entre 1-3
+    intVideoHasard: Math.floor(Math.random() * 3 + 1),
+
+    //Chercher l'ID de la video et créer le balisage source
+    refVideo: document.querySelector('#video'),
+    refSource: document.createElement('source'),
+
+    // Variable qui contiene le lien des videos
+    strUrlVideo: null,
+
     //Méthode :
     /**
      * Choisir au hasard une vidéo quand la page initialise
-     */
+    */
     jouerVideo: function () {
-        console.log("hello");
 
-        // Choisir un numéro au hasard entre 1-3
-        let intVideoHasard = Math.floor(Math.random() * 3 + 1);
-
-        // Variable qui contiene le lien des videos
-        let strUrlVideo = "./assets/videos/video_" + intVideoHasard + ".mp4";
-
-        //Chercher l'ID de la video et créer le balisage source
-        let refVideo = document.querySelector('#video');
-        let refSource = document.createElement('source');
+        this.strUrlVideo = "./assets/videos/video_" + this.intVideoHasard + ".mp4";
 
         //Dans le balisage source, set l'attribute du lien de la video et le type
-        refSource.setAttribute('src', strUrlVideo);
-        refSource.setAttribute('type', 'video/mp4');
+        this.refSource.setAttribute('src', this.strUrlVideo);
+        this.refSource.setAttribute('type', 'video/mp4');
 
-        refVideo.appendChild(refSource);
-
-        refVideo.play();
+        this.refVideo.appendChild(this.refSource);
+        this.refVideo.play();
+    },
+    /**
+     * Quand la video termine, chargement de la video suivante
+     */
+    videoSuivante: function () {
+        // this. ne fonctionne pas car fonction appelé par evenement.
+        objVisionneuse.intVideoHasard = (++objVisionneuse.intVideoHasard % 3) + 1;
+        objVisionneuse.refVideo.firstElementChild.src = "./assets/videos/video_" + objVisionneuse.intVideoHasard + ".mp4";
+        objVisionneuse.refVideo.load();
+        objVisionneuse.refVideo.play();
     }
 
 };
@@ -45,3 +55,4 @@ objVisionneuse.jouerVideo();
 
 // Assignation des écouteurs d'événements :
 document.getElementById('video').addEventListener('load', objVisionneuse.jouerVideo);
+document.getElementById("video").addEventListener("ended", objVisionneuse.videoSuivante);
